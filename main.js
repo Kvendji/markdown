bccf79ab0bee8874e880e99272d62b198c4f6c47
Kvendji <52334404+Kvendji@users.noreply.github.com>
2019-11-19 19:36:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
-var request = new XMLHttpRequest 
-  requst.onreadystatechange = function (event) {
-   this.readyState === 4 ? this.status === 200 ? 
-   console.log(this.responseText) :
-     console.warn('error') : null
-  }
+function getData (url,callback) {
+  var request = Object.assign(
+    new XMLHttpRequest, {
+      onreadystatechange :function (event) {
+        this.readyState === 4 ? this.status === 200 ?
+          callback(this.responseText) :
+             console.warn('error') : null
+      }
+    }
+  )
+  request.open("GET",url)
+  request.send()
+}
 
-requst.open("GET","./user.json")
+function showMarkup (html) {
+  document.body.innerHTML +=html
+}
 
-request.send()
+function showText (text) {
+document.body.innerHTML += <h3>${text}</h3>
+}
+
+
+var showGaallery = function (response) {
+  pictures = JSON.parse(response)
+  pictures.forEach(
+      pictures => document.body.appendChild(document.createElement('img')).src = pictures.ref 
+    )
+}
+
+
+// var func = function (response) {
+//   console.log('success')
+//   console.log(response)
+// }
+
+getData("./user2.json", showGallery)
+
+getData("hello.txt", showText)
+
+getData("hello.html", showMarkup)
